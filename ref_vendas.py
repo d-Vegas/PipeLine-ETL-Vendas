0fcc9a880efbdf3eae255fc565f3cf8b50ba1e59
Lucas Silva <lucasyannsantossilva@hotmail.com>
2024-09-30 20:39:19 -0300

    feat : atualização na construção da ref_vendas

--- ref_vendas.py
+++ ref_vendas.py
@@ -6,53 +6,43 @@ def lambda_handler(event, context):
     destination_bucket = 'ref-vendas'
 
     try:
-        # Leitura dos datasets do S3
-        orders = wr.s3.read_parquet(path=f's3://{source_bucket}/olist_orders_dataset.parquet/')
-        customers = wr.s3.read_parquet(path=f's3://{source_bucket}/olist_customers_dataset.parquet/')
-        items = wr.s3.read_parquet(path=f's3://{source_bucket}/olist_order_items_dataset.parquet/')
-        payments = wr.s3.read_parquet(path=f's3://{source_bucket}/olist_order_payments_dataset.parquet/')
-        reviews = wr.s3.read_parquet(path=f's3://{source_bucket}/olist_order_reviews_dataset.parquet/')
-        products = wr.s3.read_parquet(path=f's3://{source_bucket}/olist_products_dataset.parquet/')
-        sellers = wr.s3.read_parquet(path=f's3://{source_bucket}/olist_sellers_dataset.parquet/')
-        geolocation = wr.s3.read_parquet(path=f's3://{source_bucket}/olist_geolocation_dataset.parquet/')
+        # Definindo a lista de datasets e os caminhos do S3
+        datasets = {
+            "fact_order_details": f's3://{source_bucket}/fact_order_details.parquet/',
+            "dim_customers": f's3://{source_bucket}/dim_customers.parquet/',
+            "dim_products": f's3://{source_bucket}/dim_products.parquet/',
+        }
 
-        # Criando a tabela fato (`fact_orders`) a partir dos merges necessários
-        fact_orders = orders.merge(customers, on='customer_id', how='left') \
-            .merge(items, on='order_id', how='left') \
-            .merge(products, on='product_id', how='left') \
-            .merge(sellers, on='seller_id', how='left') \
-            .merge(payments, on='order_id', how='left') \
-            .merge(reviews, on='order_id', how='left') \
-            .merge(geolocation, left_on='customer_zip_code_prefix', right_on='geolocation_zip_code_prefix', how='left')
+        # Carregando a tabela fato e as dimensões necessárias
+        df_fact_order_details = wr.s3.read_parquet(datasets["fact_order_details"])
+        df_dim_products = wr.s3.read_parquet(datasets["dim_products"])
+        df_dim_customers = wr.s3.read_parquet(datasets["dim_customers"])
 
-        # Conversão de datas
-        fact_orders['order_purchase_timestamp'] = pd.to_datetime(fact_orders['order_purchase_timestamp'], errors='coerce')
-        fact_orders['order_delivered_customer_date'] = pd.to_datetime(fact_orders['order_delivered_customer_date'], errors='coerce')
-        fact_orders['order_approved_at'] = pd.to_datetime(fact_orders['order_approved_at'], errors='coerce')
+        # Removendo valores nulos específicos, conforme necessário
+        df_fact_order_details.fillna({'payment_value': 0}, inplace=True)
 
-        # Calculando colunas derivadas para a tabela fato
-        fact_orders['delivery_time_days'] = (fact_orders['order_delivered_customer_date'] - fact_orders['order_purchase_timestamp']).dt.days
-        fact_orders['total_order_value'] = fact_orders['price'] * fact_orders['order_item_id']
+        # Cálculos derivados
+        df_fact_order_details['delivery_time_days'] = (
+            pd.to_datetime(df_fact_order_details['order_delivered_customer_date'], errors='coerce') -
+            pd.to_datetime(df_fact_order_details['order_purchase_timestamp'], errors='coerce')
+        ).dt.days
 
-        # Tratando valores nulos na coluna 'payment_value'
-        fact_orders['payment_value'].fillna(0, inplace=True)
-
-        # Salvando a tabela fato
-        wr.s3.to_parquet(
-            df=fact_orders,
-            path=f's3://{destination_bucket}/fact_orders.parquet',
-            dataset=True,
-            mode='overwrite'
-        )
+        # Adicionar valor total do pedido para cada item de pedido
+        df_fact_order_details['total_order_value'] = df_fact_order_details['price']
 
         # Análise de Desempenho de Produtos
-        product_performance = fact_orders.groupby('product_id').agg(
+        product_performance = df_fact_order_details.groupby('product_id').agg(
             product_sales_volume=('order_item_id', 'count'),
             product_sales_value=('total_order_value', 'sum'),
-            product_return_rate=('order_status', lambda x: (x == 'returned').sum() / x.count()),
+            product_return_rate=('order_status', lambda x: (x == 'canceled').sum() / x.count()),
             average_review_score=('review_score', 'mean')
         ).reset_index()
 
+        # Adicionando informações da dimensão de produtos
+        product_performance = product_performance.merge(df_dim_products[['product_id', 'product_category_name']],
+                                                        on='product_id', how='left')
+
+        # Salvando a análise de desempenho de produtos no S3
         wr.s3.to_parquet(
             df=product_performance,
             path=f's3://{destination_bucket}/product_performance.parquet',
@@ -60,37 +50,14 @@ def lambda_handler(event, context):
             mode='overwrite'
         )
 
-        # Análise de Margem de Lucro (se houver dados de custo)
-        if 'cost' in fact_orders.columns:
-            fact_orders['product_margin'] = (fact_orders['price'] - fact_orders['cost']) / fact_orders['price']
-            product_margin = fact_orders[['product_id', 'product_margin']].drop_duplicates()
-
-            wr.s3.to_parquet(
-                df=product_margin,
-                path=f's3://{destination_bucket}/product_margin.parquet',
-                dataset=True,
-                mode='overwrite'
-            )
-
-        # Análise de Categorias de Produtos
-        category_performance = fact_orders.groupby('product_category_name').agg(
-            category_sales_volume=('order_item_id', 'count'),
-            category_conversion_rate=('order_status', lambda x: (x == 'delivered').sum() / x.count())
-        ).reset_index()
-
-        wr.s3.to_parquet(
-            df=category_performance,
-            path=f's3://{destination_bucket}/category_performance.parquet',
-            dataset=True,
-            mode='overwrite'
-        )
-
-        # Análise Geográfica
-        regional_sales = fact_orders.groupby(['customer_city', 'product_id']).agg(
+        # Análise Geográfica (utilizando 'customer_city' e 'customer_state')
+        regional_sales = df_fact_order_details.merge(df_dim_customers[['customer_id', 'customer_city', 'customer_state']],
+                                                     on='customer_id', how='left').groupby(['customer_state', 'customer_city', 'product_id']).agg(
             regional_sales_volume=('order_item_id', 'count'),
             regional_sales_value=('total_order_value', 'sum')
         ).reset_index()
 
+        # Salvando a análise geográfica no S3
         wr.s3.to_parquet(
             df=regional_sales,
             path=f's3://{destination_bucket}/regional_sales.parquet',
@@ -99,11 +66,12 @@ def lambda_handler(event, context):
         )
 
         # Análise de Padrões de Pagamento
-        payment_analysis = fact_orders.groupby('payment_type').agg(
+        payment_analysis = df_fact_order_details.groupby('payment_type').agg(
             payment_frequency=('payment_type', 'count'),
             average_payment_value=('payment_value', 'mean')
         ).reset_index()
 
+        # Salvando a análise de padrões de pagamento no S3
         wr.s3.to_parquet(
             df=payment_analysis,
             path=f's3://{destination_bucket}/payment_analysis.parquet',
@@ -112,11 +80,13 @@ def lambda_handler(event, context):
         )
 
         # Análise de Potenciais Fraudes
-        fraud_analysis = fact_orders.groupby('product_category_name').agg(
+        average_payment_value = df_fact_order_details['payment_value'].mean()
+        fraud_analysis = df_fact_order_details.groupby('product_category_name').agg(
             total_order_value=('total_order_value', 'sum'),
-            is_suspicious_order=('payment_value', lambda x: (x > fact_orders['payment_value'].mean() * 3).any())  # Exemplo de transações suspeitas
+            is_suspicious_order=('payment_value', lambda x: (x > average_payment_value * 3).any())
         ).reset_index()
 
+        # Salvando a análise de potenciais fraudes no S3
         wr.s3.to_parquet(
             df=fraud_analysis,
             path=f's3://{destination_bucket}/fraud_analysis.parquet',
@@ -125,11 +95,13 @@ def lambda_handler(event, context):
         )
 
         # Análise de Tempo de Entrega
-        delivery_analysis = fact_orders.groupby(['customer_city', 'product_id']).agg(
+        delivery_analysis = df_fact_order_details.merge(df_dim_customers[['customer_id', 'customer_city', 'customer_state']],
+                                                        on='customer_id', how='left').groupby(['customer_state', 'customer_city', 'product_id']).agg(
             average_delivery_time=('delivery_time_days', 'mean'),
-            delivery_delay_rate=('delivery_time_days', lambda x: (x > 5).sum() / x.count() if x.count() > 0 else 0)  # Evitar divisão por zero
+            delivery_delay_rate=('delivery_time_days', lambda x: (x > 5).sum() / x.count() if x.count() > 0 else 0)
         ).reset_index()
 
+        # Salvando a análise de tempo de entrega no S3
         wr.s3.to_parquet(
             df=delivery_analysis,
             path=f's3://{destination_bucket}/delivery_analysis.parquet',
